5a58552253ee4e631113e77108aea28e22a69c56
Jonathan Metzler <jmetzler50@stuy.edu>
2025-04-03 10:32:35 -0400

    created basic database login stuff

--- app/database.py
+++ app/database.py
@@ -1,15 +1,31 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-
+from flask_pymongo import PyMongo
 uri = "mongodb+srv://Jon_595:wH85NEbxuVV3XH1U@cluster0.7risuh5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
+db = client.userdb
+users = db.users
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+def register(username, password):
+    if users.find_one({"username": username}):
+        return {"error": "User already exists!"}
 
+    # Hash the password before storing
+    hashed_password = generate_password_hash(password, method='sha256')
+
+    # Insert new user into DB
+    users.insert_one({"username": username, "password": hashed_password})
+
+    return True
+
+# Function to log in an existing user
+def login(username, password):
+    # Find the user by username
+    user = users.find_one({"username": username})
+
+    if user and check_password_hash(user['password'], password):
+        return True
+    else:
+        return False
