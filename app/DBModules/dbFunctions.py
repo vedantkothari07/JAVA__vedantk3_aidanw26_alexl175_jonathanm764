95c2089a965f9f7e3d979d3839d975d0cdbdea16
Alex Luo <aluo50@stuy.edu>
2025-04-07 00:03:22 -0400

    addedmongo registration

--- app/DBModules/dbFunctions.py
+++ app/DBModules/dbFunctions.py
@@ -1,9 +1,10 @@
-import sqlite3
+#import sqlite3
+from pymongo import MongoClient, errors
 
-DB_FILE="America.db"
+#DB_FILE="America.db"
 
 ############################# Build Database #############################
-
+'''
 def initDB():
     db = sqlite3.connect(DB_FILE) # Open/Create database file
     c = db.cursor()
@@ -47,3 +48,29 @@ def registerUser(username, password):
         addUser(username, password)
         return True
     return False
+'''
+MONGO_URL = "mongodb+srv://alexl175:4Kqgfoufp7qErYSA@cluster0.pvbrpxs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+client = MongoClient(MONGO_URL)
+db = client["userdb"]
+users = db["users"]
+
+def initDB():
+    try: 
+        users.create_index("username", unique=True)
+    except errors.OperationFailure:
+        pass # if username already exists 
+
+def addUser(username, password):
+    users.insert_one({"username": username, "password": password})
+
+def checkUser(username, password):
+    doc = users.find_one({"username": username})   
+    if not doc:                                
+        return False
+    return doc["password"] == password    
+
+def registerUser(username, password):
+    if users.find_one({"username": username}):
+        return False              # username taken
+    addUser(username, password)
+    return True
