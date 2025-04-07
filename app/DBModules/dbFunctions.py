9c8c5bffc318c02ed8dde9d9edb8e02c96b3b8e8
Alex Luo <aluo50@stuy.edu>
2025-04-07 10:43:26 -0400

    removed

--- app/DBModules/dbFunctions.py
+++ app/DBModules/dbFunctions.py
@@ -2,25 +2,25 @@ from pymongo import MongoClient, errors
 
 ############################# Build Database #############################
 
-MONGO_URL = "mongodb+srv://alexl175:@cluster0.pvbrpxs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URL = "mongodb+srv://alexl175:4Kqgfoufp7qErYSA@cluster0.pvbrpxs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = MongoClient(MONGO_URL)
 db = client["userdb"]
 users = db["users"]
 
 def initDB():
-    try: 
+    try:
         users.create_index("username", unique=True)
     except errors.OperationFailure:
-        pass # if username already exists 
+        pass # if username already exists
 
 def addUser(username, password):
     users.insert_one({"username": username, "password": password})
 
 def checkUser(username, password):
-    doc = users.find_one({"username": username})   
-    if not doc:                                
+    doc = users.find_one({"username": username})
+    if not doc:
         return False
-    return doc["password"] == password    
+    return doc["password"] == password
 
 def registerUser(username, password):
     if users.find_one({"username": username}):
